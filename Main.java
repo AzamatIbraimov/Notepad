ca69479db28ed1681740c7f42e9368ebfb4ea31e
Azamat Ibraimov <aibraimov@pop-os.localdomain>
2021-11-11 00:53:38 +0600

    initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2020 Intern Labs O!
+ * <p>
+ * Notepad program made with Java and Swing with MVC pattern
+ * Main features:
+ * Open documents;
+ * Create documents;
+ * Save documents into text file;
+ * Print document on paper or pdf file.
+ * And a lot of other features!
+ *
+ * @author Erkin Koshoev
+ * @author Azamat Ibraimov
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /*
+          Creating new instance of view
+         */
+        Viewer viewer = new Viewer();
+
+    }
+}
